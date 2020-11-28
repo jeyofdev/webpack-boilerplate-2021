871dab60a825fb2f262953491f61568bd5ea4463
jeyofdev <jgregoire.pro@gmail.com>
2020-11-28 10:23:50 +0100

    init the plugin htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+const plugins = {
+  htmlWebpackPlugin: require('html-webpack-plugin'),
+};
+
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
@@ -8,4 +12,10 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
   },
+  plugins: [
+    new plugins.htmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'app.html',
+    }),
+  ],
 };
