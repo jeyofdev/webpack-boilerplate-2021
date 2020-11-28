7b34a3b730e607db9ec7ab947e8f2dfdfe8c84a4
jeyofdev <jgregoire.pro@gmail.com>
2020-11-28 18:31:39 +0100

    generate the manifest in production mode

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new WebpackAssetsManifest({
+      output: 'manifest.json',
+      space: 2,
+      writeToDisk: false,
+      replacer: require('./util/assetManifestsFormatter'),
+    }),
+  ],
 });
