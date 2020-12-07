aa4b4a6c2005d9e94cbdd2895bc165181f2cbda9
jeyofdev <jgregoire.pro@gmail.com>
2020-12-07 18:19:46 +0100

    add assets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,29 @@
+import Hello from './modules/Hello';
+import { HelloEN, HelloFR } from './modules/HelloWorld';
+import 'slick-carousel';
 import '../styles/app.scss';
+import data from '../data/user.json';
+import iconSvg from '../images/icon.svg';
+
+class User {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+
+const newUser = new User(data.firstname, data.lastname);
+
+const logo = document.createElement('img');
+logo.classList.add('logo');
+logo.src = iconSvg;
+logo.setAttribute('width', 50);
+logo.setAttribute('height', 50);
+document.getElementsByClassName('navbar-brand')[0].appendChild(logo);
+
+console.log(Hello('hello world'));
+console.log(HelloEN());
+console.log(HelloFR());
+
+$('p').css('color', '#64829c');
+$('.slick-slider').slick();
